233cda8d907e4eb704746a3c8b3eb2ff4d4a2ca1
marinacor1 <marinaspam1234@gmail.com>
2016-06-06 09:22:46 -0600

    Initial feature test written for searching with zip code

---
+++ spec/features/user_can_search_fuel_by_zip_code_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "user can search fuel by zip code" do
+  it "shows a list of gas stations with attributes" do
+    user = User.create(name: "Marina", api_key: ENV["#{NREL_KEY}"])
+
+    visit root_path
+
+    fill_in "Search", with: "80203"
+
+    click_on "Locate"
+
+    expect(current_path).to eq("/search?zip=80203")
+
+    within(".search_results") do
+        #expect quantity to be 10
+        #expect to only have Electric and Propane
+      expect(page).to have_content("Electric")
+      expect(page).to have_content("Propane")
+    end
+#2nd child should have this information
+    within(".search_results")[2] do
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Address")
+      expect(page).to have_content("Fuel Types")
+      expect(page).to have_content("Distance")
+      expect(page).to have_content("Access Times")
+    end
+
+  end
+end
